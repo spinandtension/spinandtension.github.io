2c8d7c8af26386aae59d1a75eab0f68977ce041f
Jack Snodgrass <stringandtension@gmail.com>
2024-09-07 23:09:41 -0400

    Email Functionality Commit

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ function sendMail(){
     var params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
-        message: document.getElementById("message").value
+        message: document.getElementById("message").value,
+        phone: document.getElementById("phone").value
     
     }
     const serviceID = "service_vziw9qr"
@@ -10,9 +11,10 @@ function sendMail(){
 
     emailjs.send(serviceID,templateID, params)
     .then((res)=>{
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value= "";
+            document.getElementById("name").value = "name";
+            document.getElementById("email").value = "email";
+            document.getElementById("message").value= "message";
+            document.getElementById("phone").value= "phone";
             console.log(rs);
             alert("your message sent successfully");
         })
